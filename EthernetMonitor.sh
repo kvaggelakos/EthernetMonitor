0c678c6bdb2faf8f8986b83404ec5ddc3fb20bbe
Konstantinos Vaggelakos <kozze89@gmail.com>
2011-08-26 13:58:57 +0200

    Added support for re-enabling wifi

--- EthernetMonitor.sh
+++ EthernetMonitor.sh
@@ -3,9 +3,16 @@ if ifconfig en0 | grep "status: active"; then
         if ifconfig en1 | grep "status: active"; then
                 networksetup -setairportpower en1 off
                 if [ -f "/usr/local/bin/growlnotify" ]; then
-                        /usr/local/bin/growlnotify -m "Shut down Wi-Fi, entering Cable mode!"
+                    /usr/local/bin/growlnotify -m "Shut down Wi-Fi, entering Cable mode!"
                 fi
         fi
+elif ifconfig en0 | grep "status: inactive"; then
+        if ifconfig en1 | grep "status: inactive"; then
+            networksetup -setairportpower en1 on
+            if [ -f "/usr/local/bin/growlnotify" ]; then
+                /usr/local/bin/growlnotify -m "Cable removed, starting Wi-fi again!"
+            fi
+        fi
 else
-        exit 0
+    exit 0
 fi
